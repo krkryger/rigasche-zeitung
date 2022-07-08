a1213f9b5f40a0021925a59163dadd55afbe6313
krkryger <krister.kruusmaa@hotmail.com>
2022-07-08 18:14:42 +0300

    New regex and date verification experiments

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,4 +1,5 @@
 import sys
+from unittest import result
 import pandas as pd
 import re
 import convertdate
@@ -23,104 +24,145 @@ with open('../data/placename_replacement_dict.json', 'r', encoding='utf8') as f:
     placename_replacement_dict = json.load(f)
 
 
+def extract_data_from_match(match):
+    m = match.groupdict()
+    if m['DATE2'] is None:
+        return [m['placename'], m['date'], m['date_par'], m['month'], m['month2'], match.span()[0], match.span()[1]]
+    elif m['DATE'] is None:
+        return [m['placename'], m['date2'], m['date2_par'], m['month2'], m['month2_par'], match.span()[0], match.span()[1]]
+
+        
 def scan_placenames_dates(main_df, exceptions):
     
     results = []
     
     for ix, row in tqdm(main_df.iterrows()):
-        matches = re.finditer(pattern, row.full_text)
+        matches = list(re.finditer(pattern, row.full_text))
         for m in matches:
-            results.append([ix, row.date] + list(m.groupdict().values()) + [m.span()[0], m.span()[1]])
+            results.append([ix, row.date] + extract_data_from_match(m))
         
-    result_df = pd.DataFrame(columns=['doc_id', 'doc_date', 'placename', 'day', 'day2', 'month', 'start', 'end'],
-                        data=results).fillna(pd.NA)
+    result_df = pd.DataFrame(columns=['doc_id', 'doc_date', 'placename', 'day', 'day2', 'month', 'month2', 'start', 'end'],
+                             data=results).fillna(pd.NA)
     
     result_df['doc_date'] = pd.to_datetime(result_df['doc_date'], format='%Y-%m-%d')
     result_df['year'] = result_df['doc_date'].dt.year
     
-    result_df['day'] = result_df['day'].astype(int)
-    result_df['day'] = result_df['day'].apply(lambda x: x if (x in range(1,32)) else pd.NA)
-    
-    result_df['day2'] = result_df['day2'].str.lstrip('( ').str.strip('.) ')
-    result_df['day2'].fillna(0, inplace=True)
-    result_df['day2'] = result_df['day2'].astype(int)
-    result_df.replace(0, pd.NA, inplace=True)
+    for col in ['day', 'day2']:
+        
+        result_df[col] = result_df[col].str.lstrip('( \n').str.strip('.)\n ')
+        result_df[col].fillna(0, inplace=True)
+        result_df[col] = result_df[col].astype(int)
+        result_df[col] = result_df[col].apply(lambda x: x if (x in range(1,32)) else pd.NA)
 
+        # try/except included because of possible pandas bug: AttributeError: 'bool' object has no attribute 'to_numpy'
+        try:
+            result_df[col].replace(0, pd.NA, inplace=True)
+        except AttributeError:
+            result_df.loc[(result_df[col] == 0), col] = pd.NA
+
+    # return the entries where placename does not fall under exceptions
     return result_df[~result_df.placename.isin(exceptions)]
 
 
 def cleanup_dates(df):
-    
-    #df = df.copy()
-    
+
+    """Formats the day/month columns for further operations and removes noise."""
+
     month_dict = dict(zip(['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun',
                            'Jul', 'Aug', 'Sept', 'Okt', 'Nov', 'Dec'], range(1,13)))
     
-    df.month = df.month.str.capitalize()
-    df.month.replace({'Dez': 'Dec', 'Mar': 'Mär', 'Oct': 'Okt', '0ct': 'Okt', '0kt': 'Okt',
-                      'Jnl': 'Jul', 'Jnn': 'Jun', 'Juu': 'Jun', 'May': 'Mai'}, inplace=True)
+    for col in ['month', 'month2']:
     
-    df.month.replace(month_dict, inplace=True)
-    df.loc[df.month.isin(['C', 'C.', 'D. m', 'D. m.']), 'month'] = df.loc[df.month.isin(['C', 'C.', 'D. m', 'D. m.']), 'doc_date'].dt.month
-    df.loc[df.month.isin(['V. m', 'V. m.']), 'month'] = (df.loc[df.month.isin(['v. M', 'V. m.']), 'doc_date'] - pd.DateOffset(months=1)).dt.month - 1
+        df[col] = df[col].str.capitalize()
+        df[col].replace({'Dez': 'Dec', 'Mar': 'Mär', 'Oct': 'Okt', '0ct': 'Okt', '0kt': 'Okt', 'Ocl': 'Oct',
+                          'Jnl': 'Jul', 'Jnn': 'Jun', 'Juu': 'Jun', 'May': 'Mai', 'Spt': 'Sept'}, inplace=True)
+        
+        df[col].replace(month_dict, inplace=True)
+        df.loc[df[col].isin(['C', 'C.', 'D. m', 'D. m.']), col] = df.loc[df[col].isin(['C', 'C.', 'D. m', 'D. m.']), 'doc_date'].dt.month
+        df.loc[df[col].isin(['V. m', 'V. m.']), 'month'] = (df.loc[df[col].isin(['v. M', 'V. m.']), 'doc_date'] - pd.DateOffset(months=1)).dt.month - 1
+        
+        df[col] = df[col].fillna(0).astype(int)
+        df[col] = df[col].apply(lambda x: x if (x in range(1,13)) else pd.NA)
     
-    df.month = df.month.fillna(0).astype(int)
-    df.month = df.month.apply(lambda x: x if (x in range(1,13)) else pd.NA)
-    
-    df.day = df.day.fillna(0).astype(int)
-    df.day = df.day.apply(lambda x: x if (x in range(1,32)) else pd.NA)
-    
-    df.day2 = df.day2.fillna(0).astype(int)
-    df.day2 = df.day2.apply(lambda x: x if (x in range(1,32)) else pd.NA)
-    
-    return df   
+    return df    
 
 
 def verify_dates(ix, df):
     
-    #print(df.loc[ix,['day', 'day2', 'month', 'doc_date']])
+    """Function to find the logically correct date from the information found by the regular expression.
+    Has different approaches depending on the number of dates and months detected. In the case of multiple
+    possibilities, returns the earlier one (corresponding to the Julian calendar)."""
     
-    day, day2, month, doc_date = df.loc[ix,['day', 'day2', 'month', 'doc_date']]
+    #print(df.loc[ix,['day', 'day2', 'month', 'doc_date']].values)
     
-    if type(day) == pd._libs.missing.NAType or type(month) == pd._libs.missing.NAType:
+    day, day2, month, month2, doc_date = [value if type(value) != pd._libs.missing.NAType else None
+                                          for value in df.loc[ix,['day', 'day2', 'month', 'month2', 'doc_date']].values]
+    
+    # if 
+    if day is None or month is None:
         return pd.NA
     
-    # 2 kp
-    if type(day2) == int:
-        #print('difference: ', max(day, day2) - min(day, day2))
-        if month == doc_date.month or month < doc_date.month:
-            if day > day2 and day > doc_date.day:
-                # julian is the smaller one
-                origin_date = pd.to_datetime(f'{str(doc_date.year)}-{month}-{day2}',
-                                             format='%Y-%m-%d', errors='coerce')
-            else: # kontrolli see üle
-                origin_date = pd.to_datetime(f'{str(doc_date.year)}-{month}-{day}',
-                                             format='%Y-%m-%d', errors='coerce')
-                
-        elif month > doc_date.month:
-            if month == 12 and doc_date.month == 1:
-                if day > day2 and day > doc_date.day:
-                    origin_date = pd.to_datetime(f'{str(doc_date.year-1)}-{month}-{day2}',
-                                                 format='%Y-%m-%d', errors='coerce')
-                else:
-                    origin_date = pd.to_datetime(f'{str(doc_date.year-1)}-{month}-{day}',
-                                             format='%Y-%m-%d', errors='coerce')
+    # two dates, two months
+    if None not in [day, day2, month, month2]:
+        
+        # exception for december
+        if (month == 12 or month2 == 12) and doc_date.month == 1:
+            possible_origin_dates = []
+            for m in [month, month2]:
+                origin_year = doc_date.year-1 if m == 12 else doc_date.year
+                possible_origin_dates.append(
+                        pd.to_datetime(f'{str(origin_year)}-{m}-{day}', format='%Y-%m-%d', errors='coerce'))
                     
+            origin_date = min(possible_origin_dates)
+        
+        # normal proceeding, no december in any of the two dates
+        else:
+            # julian is the smaller one
+            origin_date = min(
+                pd.to_datetime(f'{str(doc_date.year)}-{month}-{day}', format='%Y-%m-%d', errors='coerce'),
+                pd.to_datetime(f'{str(doc_date.year)}-{month2}-{day2}', format='%Y-%m-%d', errors='coerce')
+            )
+       
+    
+    # two dates, one month
+    elif day and day2 and month:
+        
+        # if month is the same or precedes the publication month
+        if month == doc_date.month or month < doc_date.month:
+            
+            # julian is the smaller one
+            origin_date = min(
+                pd.to_datetime(f'{str(doc_date.year)}-{month}-{day2}', format='%Y-%m-%d', errors='coerce'),
+                pd.to_datetime(f'{str(doc_date.year)}-{month}-{day}', format='%Y-%m-%d', errors='coerce')
+            )
+
+        # if month follows publication month
+        elif month > doc_date.month:
+            
+            # exception for december
+            if month == 12 and doc_date.month == 1:
+                origin_date = min(
+                    pd.to_datetime(f'{str(doc_date.year-1)}-{month}-{day2}', format='%Y-%m-%d', errors='coerce'),
+                    pd.to_datetime(f'{str(doc_date.year-1)}-{month}-{day}', format='%Y-%m-%d', errors='coerce'))                 
             else:
                 return pd.NA
-                
+        
             
-    
-    # 1 kp
-    else:
-        # mis on kuu võrreldes lehe kuuga?
+    # one day, one month
+    elif day and month:
+        
+        # if month precedes publication month
         if month < doc_date.month:
-            # origin is julian, apply directly to datetime
+            # (probably) julian
             origin_date = pd.to_datetime(f'{str(doc_date.year)}-{month}-{day}',
                                          format='%Y-%m-%d', errors='coerce')
         
+        # if month is the same as publication month
         elif month == doc_date.month:
+            
+            # if the day is later than the publication day, within the same month
             if day > doc_date.day:
+                
                 # origin is gregorian, convert to julian first, then apply
                 try:
                     day_jul = convertdate.julian.from_gregorian(year=doc_date.year, month=month, day=day)
@@ -128,16 +170,23 @@ def verify_dates(ix, df):
                     return pd.NA
                 origin_date = pd.to_datetime(f'{str(day_jul[0])}-{str(day_jul[1])}-{str(day_jul[2])}',
                                              format='%Y-%m-%d', errors='coerce')
+            
+            # if the day precedes the publication day, within the same month
             else:
                 # origin is julian, apply directly to datetime
                 origin_date = pd.to_datetime(f'{str(doc_date.year)}-{month}-{day}',
                                              format='%Y-%m-%d', errors='coerce')
         
+        
+        # if month is greater than publication month
         elif month > doc_date.month:
+            
+            # exception for december
             if month == 12 and doc_date.month == 1:
                 # origin is from last year - probably in julian b/c is smaller that doc_date and there is no day2
                 origin_date = pd.to_datetime(f'{str(doc_date.year-1)}-{month}-{day}',
                                              format='%Y-%m-%d', errors='coerce')
+                
             else:
                 # origin is from next month, thus in gregorian
                 try:
@@ -172,10 +221,11 @@ df['origin_date'] = pd.to_datetime(df.origin_date)
 df['delta'] = (df['doc_date'] - df['origin_date']).dt.days
 
 # drop entries with origin date that is negative or more than 1 year
-df = df.loc[(df.delta > 0) & (df.delta < 350)] 
+print(f'Dropping {len(df) - len(df.loc[(df.delta > 0) & (df.delta < 350)])} entries with invalid (x < 0 | x > 350) time differences')
+#df = df.loc[(df.delta > 0) & (df.delta < 350)] 
 
 print('Saving the dataframe')
-df.to_csv('../data/processed_data_sample.tsv', sep='\t', encoding='utf8', index=False)
+df.to_csv('../data/processed_data_2.tsv', sep='\t', encoding='utf8', index=False)
 
 print('Finished')
 
